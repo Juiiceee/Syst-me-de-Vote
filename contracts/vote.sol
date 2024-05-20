c79612b7b6955ac17533f2a46fc036c55f2021d2
Juiiceee <lbehr@student.42.fr>
2024-05-20 16:53:31 +0200

    init

---
+++ contracts/vote.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract vote {
+    struct voting {
+        uint count_for;
+        uint count_against;
+        string description;
+    }
+    
+}
