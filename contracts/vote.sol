8e13a9cd1fe07594c25de7dce35b344fa95fea97
Juiiceee <lbehr@student.42.fr>
2024-05-31 13:22:31 +0200

    add create and display

--- contracts/vote.sol
+++ contracts/vote.sol
@@ -7,5 +7,17 @@ contract vote {
         uint count_against;
         string description;
     }
-    
+
+    voting[] public tab;
+
+    function createVote(string memory _description) public {
+        tab.push(voting(0, 0, _description));
+    }
+
+    function display(uint _index) public view returns(uint, uint, string memory) {
+        require(_index < tab.length, "out of bound");
+        return (tab[_index].count_for, tab[_index].count_against, tab[_index].description);
+    }
+
+
 }
